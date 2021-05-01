1b41fb6c6e7c865447824eaffce0dff8c7efafa4
Fahd <fahd@31256@gmail.com>
2021-05-01 22:33:51 +0000

    changed stb image to cpp

--- src/Utils/Image.cpp
+++ src/Utils/Image.cpp
@@ -2,7 +2,7 @@
 #include "Utils.hpp"
 
 #define STB_IMAGE_IMPLEMENTATION
-#include "stb_image.h"
+#include "stb_image.hpp"
 
 
 void Image::loadFromFile(const std::string& path, int desiredChannels){
