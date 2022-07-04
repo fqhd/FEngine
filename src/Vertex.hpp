8ab7e5f602395f536ba6222b02599ef25aaff23e
fqhd <fahd31256@gmail.com>
2022-07-04 20:27:19 +0100

    fix: add vertex class

---
+++ src/Vertex.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <glm/glm.hpp>
+
+class Vertex {
+public:
+    Vertex(glm::vec3 p, glm::vec3 n, glm::vec2 t){
+        position = p;
+        normal = n;
+        uv = t;
+    }
+    glm::vec3 position;
+    glm::vec3 normal;
+    glm::vec2 uv;
+};
