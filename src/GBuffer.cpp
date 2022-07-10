0bb111f5f38506f11dea2ae4c064d98e07f67319
Fahd <fahd31256@gmail.com>
2022-07-10 12:26:34 +0100

    fix: gbuffer
    add gbuffer for deferred rendering calculations

---
+++ src/GBuffer.cpp
@@ -0,0 +1,91 @@
+#include "GBuffer.hpp"
+#include <iostream>
+
+void GBuffer::init(unsigned int width, unsigned int height)
+{
+    glGenFramebuffers(1, &m_fboID);
+    glBindFramebuffer(GL_FRAMEBUFFER, m_fboID);
+
+    // - position color buffer
+    glGenTextures(1, &m_positionTextureID);
+    glBindTexture(GL_TEXTURE_2D, m_positionTextureID);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA16F, width, height, 0, GL_RGB, GL_FLOAT, nullptr);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+    glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, m_positionTextureID, 0);
+    glBindTexture(GL_TEXTURE_2D, 0);
+
+    // - normal color buffer
+    glGenTextures(1, &m_normalTextureID);
+    glBindTexture(GL_TEXTURE_2D, m_normalTextureID);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA16F, width, height, 0, GL_RGB, GL_FLOAT, nullptr);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT1, GL_TEXTURE_2D, m_normalTextureID, 0);
+    glBindTexture(GL_TEXTURE_2D, 0);
+
+    // - color + specular color buffer
+    glGenTextures(1, &m_albedoTextureID);
+    glBindTexture(GL_TEXTURE_2D, m_albedoTextureID);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, nullptr);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT2, GL_TEXTURE_2D, m_albedoTextureID, 0);
+    glBindTexture(GL_TEXTURE_2D, 0);
+
+    // - tell OpenGL which color attachments we'll use (of this framebuffer) for rendering
+    unsigned int attachments[3] = {GL_COLOR_ATTACHMENT0, GL_COLOR_ATTACHMENT1, GL_COLOR_ATTACHMENT2};
+    glDrawBuffers(3, attachments);
+
+    // Depth buffer
+    glGenRenderbuffers(1, &m_rboID);
+    glBindRenderbuffer(GL_RENDERBUFFER, m_rboID);
+
+    glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH24_STENCIL8, width, height);
+    glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_STENCIL_ATTACHMENT, GL_RENDERBUFFER, m_rboID);
+
+    glBindRenderbuffer(GL_RENDERBUFFER, 0);
+
+    if (glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE)
+    {
+        std::cout << "Failed to create GBuffer Framebuffer" << std::endl;
+    }
+
+    glBindFramebuffer(GL_FRAMEBUFFER, 0);
+}
+
+void GBuffer::clear()
+{
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+}
+
+void GBuffer::bind()
+{
+    glBindFramebuffer(GL_FRAMEBUFFER, m_fboID);
+}
+
+void GBuffer::unbind()
+{
+    glBindFramebuffer(GL_FRAMEBUFFER, 0);
+}
+
+void GBuffer::destroy()
+{
+    glDeleteTextures(1, &m_positionTextureID);
+    glDeleteTextures(1, &m_albedoTextureID);
+    glDeleteTextures(1, &m_normalTextureID);
+    glDeleteRenderbuffers(1, &m_rboID);
+    glDeleteFramebuffers(1, &m_fboID);
+}
+
+GLuint GBuffer::getPositionTextureID()
+{
+    return m_positionTextureID;
+}
+
+GLuint GBuffer::getNormalTextureID()
+{
+    return m_normalTextureID;
+}
---
+++ src/GBuffer.hpp
@@ -0,0 +1,23 @@
+#include <GL/glew.h>
+#include <string>
+
+class GBuffer
+{
+public:
+    void init(unsigned int width, unsigned int height);
+    void bind();
+    void unbind();
+    void destroy();
+    void clear();
+
+    GLuint getPositionTextureID();
+    GLuint getAlbedoTextureID();
+    GLuint getNormalTextureID();
+
+private:
+    GLuint m_fboID = 0;
+    GLuint m_rboID = 0;
+    GLuint m_positionTextureID = 0;
+    GLuint m_albedoTextureID = 0;
+    GLuint m_normalTextureID = 0;
+};
