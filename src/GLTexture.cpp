32398941eac4f2b0d2ccaf31496735a979056529
Fahd <fahd31256@gmail.com>
2022-07-11 20:56:49 +0100

    Mipmapless Nearest Texture Filtering

--- src/GLTexture.cpp
+++ src/GLTexture.cpp
@@ -10,15 +10,14 @@ void GLTexture::init(const char *path, Color color)
 
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR_MIPMAP_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
 
     // Loading in data
     Image image;
     if (image.loadFromFile(path) == 1)
     {
         glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, image.getWidth(), image.getHeight(), 0, GL_RGBA, GL_UNSIGNED_BYTE, image.getData());
-        glGenerateMipmap(GL_TEXTURE_2D);
     }
     else
     {
