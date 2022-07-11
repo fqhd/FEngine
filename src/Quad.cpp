cbc6ded84bee7cf9a67895e2d2c54ae00668ca6b
Fahd <fahd31256@gmail.com>
2022-07-11 20:44:36 +0100

    Drawing quad with 0, 0 parameters

--- src/Quad.cpp
+++ src/Quad.cpp
@@ -17,7 +17,7 @@ void Quad::init(){
     };
 
     glEnableVertexAttribArray(0);
-    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, 0);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
     glBindBuffer(GL_ARRAY_BUFFER, 0);
