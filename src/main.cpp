59b119d436d04f9c61196e8359575e4314c379e2
Fahd <fahd31256@gmail.com>
2022-07-12 07:46:47 +0100

    Add camera controls

--- src/main.cpp
+++ src/main.cpp
@@ -11,7 +11,7 @@ int main()
     engine.objects.push_back(plane);
 
     engine.objects[0].transform.position.y = 1;
-    engine.objects[0].transform.position.z = 5;
+    engine.objects[0].transform.position.z = -50;
     engine.objects[1].transform.scale = glm::vec3(10);
 
     engine.camera.position.z = -5;
@@ -19,9 +19,42 @@ int main()
     engine.camera.yaw = 90;
     engine.camera.pitch = -15;
     
+    float speed = 1.1;
     while (engine.window.isOpen())
     {
-        engine.objects[0].transform.rotation.y += 1;
+        if(engine.inputManager.isKeyDown(GLFW_KEY_W)){
+            engine.camera.position.z += speed;
+        }
+        if(engine.inputManager.isKeyDown(GLFW_KEY_A)){
+            engine.camera.position.x += speed;
+        }
+        if(engine.inputManager.isKeyDown(GLFW_KEY_S)){
+            engine.camera.position.z -= speed;
+        }
+        if(engine.inputManager.isKeyDown(GLFW_KEY_D)){
+            engine.camera.position.x -= speed;
+        }
+        if(engine.inputManager.isKeyDown(GLFW_KEY_SPACE)){
+            engine.camera.position.y += speed;
+        }
+        if(engine.inputManager.isKeyDown(GLFW_KEY_LEFT_SHIFT)){
+            engine.camera.position.y -= speed;
+        }
+
+        if(engine.inputManager.isKeyDown(GLFW_KEY_UP)){
+            engine.camera.pitch += speed * 7.0;
+        }
+        if(engine.inputManager.isKeyDown(GLFW_KEY_DOWN)){
+            engine.camera.pitch -= speed * 7.0;
+        }
+        if(engine.inputManager.isKeyDown(GLFW_KEY_RIGHT)){
+            engine.camera.yaw += speed * 7.0;
+        }
+        if(engine.inputManager.isKeyDown(GLFW_KEY_LEFT)){
+            engine.camera.yaw -= speed * 7.0;
+        }
+
+        // engine.objects[0].transform.position.z -= 1;
         engine.draw();
     }
 
